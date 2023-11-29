07486c204cdbb99cbd7df5710cee6633a256af3c
Konstantin <krollex2012@gmail.com>
2023-11-29 12:16:05 +0300

    course page updates

--- src/CoursePage.tsx
+++ src/CoursePage.tsx
@@ -1,7 +1,12 @@
 import { FC, useEffect, useState } from 'react'
+import { Button, Card } from 'react-bootstrap'
+
 import './CoursePage.css'
+
 import { getCourseByName } from './modules/get-course-by-name'
 import { Course } from './modules/ds'
+
+import defaultImage from '../public/vite.svg'
 
 const CoursePage: FC = () => {
 
@@ -10,32 +15,37 @@ const CoursePage: FC = () => {
     useEffect(() => {
         const queryString = window.location.search;
         const urlParams = new URLSearchParams(queryString)
-        const courseName = urlParams.get('course_title')
+        const courseTitle = urlParams.get('course_title')
     
         const loadCourse = async () => {
-            const result = await getCourseByName(String(courseName))
-            console.log(result)
+            const result = await getCourseByName(String(courseTitle))
             setCourse(result)
         }
     
         loadCourse()
 
     }, []);
-    
+
     return (
-        <div>
-            <img src={"data:image/jpg;base64, " + course?.Image} className="card_image" />
-            <p>{course?.Description}</p>
-            <p className="course_line"><b>Статус курса:</b> { course?.Status }</p>
-            <p className="course_line"><b>Место:</b> { course?.Location }</p>
-            <p className="course_line"><b>Количество мест:</b> { course?.Capacity } чел.</p>
-            <p className="course_line"><b>Зарегистрировано:</b> { course?.Enrolled } чел.</p>
-            <p className="course_line"><b>Курс проводит:</b> { course?.CoachName }</p>
-            <p className="course_line"><b>E-Mail:</b> { course?.CoachEmail }</p>
-            <p className="course_line"><b>Телефон:</b> { course?.CoachPhone }</p>
-            <p className="course_line"><b>{ course?.Description }</b></p>
-            <a className="button page_button" href="..">Домой</a>
-        </div>
+        <div className='card_container'>
+            <Card className='page_card'>
+                <Card.Img src={(course?.Image == '' ? defaultImage?.toString() : "data:image/jpg;base64, " + course?.Image)} className="card-img-top" variant="top"/>
+                <Card.Body>
+                    <p>{ course?.Description }</p>
+                    <p><b>Статус курса:</b> { course?.Status }</p>
+                    <p><b>Место:</b> { course?.Location }</p>
+                    <p><b>Количество мест:</b> { course?.Capacity } чел.</p>
+                    <p><b>Зарегистрировано:</b> { course?.Enrolled } чел.</p>
+                    <p><b>Курс проводит:</b> { course?.CoachName }</p>
+                    <p><b>E-Mail:</b> { course?.CoachEmail }</p>
+                    <p><b>Телефон:</b> { course?.CoachPhone }</p>
+                    <p><b>{ course?.Description }</b></p>
+                </Card.Body>
+                <Card.Footer>
+                    <Button href="/">Домой</Button>
+                </Card.Footer>
+            </Card>
+        </div>        
     )
 }
 
